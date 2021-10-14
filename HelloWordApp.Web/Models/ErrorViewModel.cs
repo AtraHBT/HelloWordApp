a9776b548844e54b3f7e292affce47a00a15677b
AtraHBT <heber.belluzzo@atrainformatica.com.br>
2021-10-14 11:47:51 -0300

    Initial commit

---
+++ HelloWordApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloWordApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
